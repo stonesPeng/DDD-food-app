2bf443350a6d6f3ad83a46628a0873a905129ca0
honor_stone <honor_stone@163.com>
2021-07-09 17:15:22 +0800

    add login_handler

---
+++ intefaces/login_handler.go
@@ -0,0 +1,166 @@
+/**
+  @author: honor
+  @since: 2021/7/9
+  @desc: //TODO
+**/
+package intefaces
+
+import (
+	"DDD-food-app/application"
+	"DDD-food-app/domain/entity"
+	"DDD-food-app/infra/auth"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Authenticate struct {
+	userInterface application.UserAppInterface
+	authService   auth.AuthInterface
+	tokenService  auth.TokenInterface
+}
+
+func NewAuthenticate(userInterface application.UserAppInterface, authService auth.AuthInterface, tokenService auth.TokenInterface) *Authenticate {
+	return &Authenticate{userInterface: userInterface, authService: authService, tokenService: tokenService}
+}
+
+/**
+ * @Description: 登录鉴权
+ * @receiver authenticate
+ * @param c
+ */
+func (authenticate Authenticate) Login(c *gin.Context) {
+	var user *entity.User
+	var tokenErr = map[string]string{}
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+	//validate request:
+	validateUser := user.Validate("login")
+	if len(validateUser) > 0 {
+		c.JSON(http.StatusUnprocessableEntity, validateUser)
+		return
+	}
+	u, userErr := authenticate.userInterface.GetUserByEmailAndPassword(user)
+	if userErr != nil {
+		c.JSON(http.StatusInternalServerError, userErr)
+		return
+	}
+	ts, tErr := authenticate.tokenService.CreateToken(u.ID)
+	if tErr != nil {
+		tokenErr["token_error"] = tErr.Error()
+		c.JSON(http.StatusUnprocessableEntity, tErr.Error())
+		return
+	}
+	saveErr := authenticate.authService.CreateAuth(u.ID, ts)
+	if saveErr != nil {
+		c.JSON(http.StatusInternalServerError, saveErr.Error())
+		return
+	}
+	userData := make(map[string]interface{})
+	userData["access_token"] = ts.AccessToken
+	userData["refresh_token"] = ts.RefreshToken
+	userData["id"] = u.ID
+	userData["first_name"] = u.FirstName
+	userData["last_name"] = u.LastName
+
+	c.JSON(http.StatusOK, userData)
+}
+
+/**
+ * @Description:  退出登录
+ * @receiver authenticate
+ * @param c
+ */
+func (authenticate *Authenticate) Logout(c *gin.Context) {
+	//check is the user is authenticated first
+	metadata, err := authenticate.tokenService.ExtractTokenMetadata(c.Request)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+	//if the access token exist and it is still valid, then delete both the access token and the refresh token
+	deleteErr := authenticate.authService.DeleteTokens(metadata)
+	if deleteErr != nil {
+		c.JSON(http.StatusUnauthorized, deleteErr.Error())
+		return
+	}
+	c.JSON(http.StatusOK, "Successfully logged out")
+}
+
+/**
+ * @Description: 刷新token有效期
+ * @receiver authenticate
+ * @param c
+ */
+//Refresh is the function that uses the refresh_token to generate new pairs of refresh and access tokens.
+func (authenticate *Authenticate) Refresh(c *gin.Context) {
+	mapToken := map[string]string{}
+	if err := c.ShouldBindJSON(&mapToken); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	refreshToken := mapToken["refresh_token"]
+
+	//verify the token
+	token, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("REFRESH_SECRET")), nil
+	})
+	//any error may be due to token expiration
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, err.Error())
+		return
+	}
+	//is token valid?
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		c.JSON(http.StatusUnauthorized, err)
+		return
+	}
+	//Since token is valid, get the uuid:
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		refreshUuid, ok := claims["refresh_uuid"].(string) //convert the interface to string
+		if !ok {
+			c.JSON(http.StatusUnprocessableEntity, "Cannot get uuid")
+			return
+		}
+		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			c.JSON(http.StatusUnprocessableEntity, "Error occurred")
+			return
+		}
+		//Delete the previous Refresh Token
+		delErr := authenticate.authService.DeleteRefresh(refreshUuid)
+		if delErr != nil { //if any goes wrong
+			c.JSON(http.StatusUnauthorized, "unauthorized")
+			return
+		}
+		//Create new pairs of refresh and access tokens
+		ts, createErr := authenticate.tokenService.CreateToken(userId)
+		if createErr != nil {
+			c.JSON(http.StatusForbidden, createErr.Error())
+			return
+		}
+		//save the tokens metadata to redis
+		saveErr := authenticate.authService.CreateAuth(userId, ts)
+		if saveErr != nil {
+			c.JSON(http.StatusForbidden, saveErr.Error())
+			return
+		}
+		tokens := map[string]string{
+			"access_token":  ts.AccessToken,
+			"refresh_token": ts.RefreshToken,
+		}
+		c.JSON(http.StatusCreated, tokens)
+	} else {
+		c.JSON(http.StatusUnauthorized, "refresh token expired")
+	}
+}
